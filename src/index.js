c4038d0a0d5f5de1160ea618528994f837adbe5d
hafida <oumnawfal@gmail.com>
2017-02-27 21:38:14 +0100

    refactorisation routes

--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import PhotoGrid from './components/PhotoGrid';
-import Single from './components/Single';
-import NotFound from './components/NotFound'; 
 import './styles/style.css';
 
-
-// Dependance de react-router
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-import { Provider } from 'react-redux';
-import store from './store';
-
-
-const router = (
-<Provider store={store}>  
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="view/:postId" component={Single}></Route>
-      <Route path="*" component={ NotFound } />
-    </Route>
-  </Router> 
-</Provider>
-)
+// Router
+import router from './routes';
 
 
 ReactDOM.render(router, document.getElementById('root'));
